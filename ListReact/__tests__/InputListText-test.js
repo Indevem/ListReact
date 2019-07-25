7c880bb6a44edaabe4aebc51fc099673fe2385f8
Daria <7224181@mail.ru>
2019-07-25 12:47:28 +0300

    More tests

--- ListReact/__tests__/InputListText-test.js
+++ ListReact/__tests__/InputListText-test.js
@@ -3,7 +3,7 @@ import { Text } from 'react-native';
 import InputListText from '../components/InputListText';
 import renderer from 'react-test-renderer';
 
-test('Корректный рендер скрытого списка', () => {
+test('Корректный рендер скрытого списка (несколько элементов)', () => {
   const snap1 = renderer.create(<InputListText
     Items = {['A', 'AB', 'CA', 'D']}
     Render = {item => (<Text>{item}</Text>)}
@@ -11,7 +11,15 @@ test('Корректный рендер скрытого списка', () => {
   expect(snap1).toMatchSnapshot();
 });
 
-test('Корректный рендер раскрытого списка', () => {
+test('Корректный рендер скрытого списка (элементы отсутствуют)', () => {
+  const snap1 = renderer.create(<InputListText
+    Items = {[]}
+    Render = {item => (<Text>{item}</Text>)}
+    />).toJSON();
+  expect(snap1).toMatchSnapshot();
+});
+
+test('Корректный рендер раскрытого списка (несколько элементов)', () => {
   let ListData = renderer.create(<InputListText
     Items = {['A', 'AB', 'CA', 'D']}
     Render = {item => (<Text>{item}</Text>)}
@@ -22,19 +30,41 @@ test('Корректный рендер раскрытого списка', () =
   expect(snap2).toMatchSnapshot();
 });
 
-it('Фукция выбраного значения из предложенных', () => {
+test('Корректный рендер раскрытого списка (элементы отсутствуют)', () => {
   let ListData = renderer.create(<InputListText
     Items = {[]}
     Render = {item => (<Text>{item}</Text>)}
+    />);
+  ListData.getInstance().setState(() => ({ showList: true }));
+  ListData.getInstance().onTextChanged("A");
+  const snap2 = ListData.toJSON();
+  expect(snap2).toMatchSnapshot();
+});
+
+it('Фукция выбраного значения из предложенных', () => {
+  let ListData = renderer.create(<InputListText
+    Items = {['A', 'AB', 'CA', 'D']}
+    Render = {item => (<Text>{item}</Text>)}
     />).getInstance();
-  ListData.suggestionSelected("AAA");
-  expect(ListData.state.selectedValue).toBe("AAA");
+  ListData.suggestionSelected("A");
+  expect(ListData.state.selectedValue).toBe("A");
   expect(ListData.state.valueType).toBe(false);
 });
 
-it('Функция ввода текста пользователем', () => {
+it('Функция ввода текста пользователем (при раскрытом списке)', () => {
   let ListData = renderer.create(<InputListText
-    Items = {[]}
+    Items = {['A', 'AB', 'CA', 'D']}
+    Render = {item => (<Text>{item}</Text>)}
+    />).getInstance();
+  ListData.setState(() => ({ showList: true }));
+  ListData.onTextChanged("AAA");
+  expect(ListData.state.selectedValue).toBe("AAA");
+  expect(ListData.state.valueType).toBe(true);
+});
+
+it('Функция ввода текста пользователем (при скрытом списке)', () => {
+  let ListData = renderer.create(<InputListText
+    Items = {['A', 'AB', 'CA', 'D']}
     Render = {item => (<Text>{item}</Text>)}
     />).getInstance();
   ListData.onTextChanged("AAA");
